d1c8cdbe5a2ed4d60d8387d5b902caa0190ba38e
Krzysztof <krzysztof@engineer.com>
2025-07-19 16:09:59 +0200

    add elapsed time height

--- check.go
+++ check.go
@@ -26,3 +26,30 @@ func (c Check) ColorClass() string {
 		return "bg-neutral-300"
 	}
 }
+
+func (c Check) HeightClass(maxTime int64) string {
+	height := int64(c.ResponseTimeMs) * 100 / maxTime
+	if height < 10 {
+		return "h-[10%]"
+	} else if height < 20 {
+		return "h-[20%]"
+	} else if height < 30 {
+		return "h-[30%]"
+	} else if height < 40 {
+		return "h-[40%]"
+	} else if height < 50 {
+		return "h-[50%]"
+	} else if height < 60 {
+		return "h-[60%]"
+	} else if height < 70 {
+		return "h-[70%]"
+	} else if height < 80 {
+		return "h-[80%]"
+	} else if height < 90 {
+		return "h-[90%]"
+	} else if height < 100 {
+		return "h-full"
+	} else {
+		return "h-full"
+	}
+}
